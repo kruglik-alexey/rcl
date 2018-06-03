d25db34f33a07afe9e3cfb204b5d0928c0ea3ec8
kruglik <seldoff@gmail.com>
2018-06-03 23:24:42 +0300

    Implementing arrays and funcs diagnostics

--- rcl/equality.js
+++ rcl/equality.js
@@ -12,6 +12,42 @@ export const equalityDescriptions = {
     [DEEP_EQ]: 'deeply equal'
 };
 
+function getMatchingNotEqualKeys(a, b, matcher) {
+    const result = [];
+
+    var keysA = Object.keys(objA);
+    var keysB = Object.keys(objB);
+
+    for (var i = 0; i < keysA.length; i++) {
+        const key = keysA[i];
+        if (a[key] !== undefined && b[key] !== undefined && a[key] !== b[key]) {
+            if (matcher(a[key], b[key])) {
+                result.push(key);
+            }
+        }
+    }
+
+    return result;
+}
+
+export function getNotEqualFuntionsKeys(a, b) {
+    return getMatchingNotEqualKeys(a, b, (x, y) => typeof x === 'Function' && y === 'Function');
+}
+
+export function getArraysWithEqualContentKeys(a, b) {
+    return getMatchingNotEqualKeys(a, b, (x, y) => {
+        if (!Array.isArray(a[key]) || !Array.isArray(b[key]) || a[key].length !== b[key].length) {
+            return false;
+        }
+
+        for (var j = 0; j < a[key].length; j++) {
+            if (a[key][j] !== b[key][j]) return false;
+        }
+
+        return true;
+    });
+}
+
 export function checkEquality(a, b) {
     if (a === b) return STRICT_EQ;
     if (shallowequal(a, b)) return SHALLOW_EQ;
