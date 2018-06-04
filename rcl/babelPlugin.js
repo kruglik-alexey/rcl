0fc5ae7ce8fbc664ce70d6f353bfd73cabcd1991
kruglik <seldoff@gmail.com>
2018-06-05 00:51:24 +0300

    Improve Id generation in plugin

--- rcl/babelPlugin.js
+++ rcl/babelPlugin.js
@@ -1,7 +1,21 @@
+const template = require('babel-template');
+
+const idPropName = '__rclId__';
+const rclCreateElementName = '__createElement';
+
 let instanceId = 0;
+
+const getParentIdExpression = template(
+    `const VAR = (arguments[0] === undefined ? this.props : arguments[0]).${idPropName} + '_';`
+);
+
+const getIdExpression = template('VAR + ID');
 
 module.exports = function(babel) {
     const t = babel.types;
+    const rclCreateElementNameIdentifier = t.identifier(rclCreateElementName);
+
+    const processedFuncs = new Map();
 
     return {
         visitor: {
@@ -14,12 +28,66 @@ module.exports = function(babel) {
                     n.callee.object.name === 'React' &&
                     n.callee.property.name === 'createElement';
                 if (isCreateCompositeElement) {
+                    const parentFunc = path.find(p =>
+                        p.isClassMethod() ||
+                        p.isObjectMethod() ||
+                        p.isFunctionExpression() ||
+                        p.isFunctionDeclaration() ||
+                        p.isArrowFunctionExpression()
+                    );
+                    if (!parentFunc) {
+                        console.warn('Can\'t find enclosing funcion');
+                        return;
+                    }
+
+                    let parentIdVarName = processedFuncs.get(parentFunc);
+                    if (!parentIdVarName) {
+                        const n = parentFunc.node;
+                        const params = n.params;
+                        parentIdVarName = parentFunc.scope.generateUidIdentifier('thisId');
+                        processedFuncs.set(parentFunc, parentIdVarName);
+                        const parentIdExpression = getParentIdExpression({VAR: parentIdVarName});
+
+                        if (t.isBlockStatement(n.body)) {
+                            const newBody = t.blockStatement([parentIdExpression].concat(n.body.body));
+
+                            let r;
+                            if (parentFunc.isClassMethod()) {
+                                r = t.classMethod(n.kind, n.key, params, newBody, n.computed, n.strict);
+                            } else if (parentFunc.isObjectMethod()) {
+                                r = t.objectMethod(n.kind, n.key, params, newBody, n.computed);
+                            } else if (parentFunc.isFunctionExpression()) {
+                                r = t.functionExpression(n.id, params, newBody, n.generator, n.async);
+                            } else if (parentFunc.isFunctionDeclaration()) {
+                                r = t.functionDeclaration(n.id, params, newBody, n.generator, n.async);
+                            } else if (parentFunc.isArrowFunctionExpression()) {
+                                r = t.arrowFunctionExpression(params, newBody);
+                            } else {
+                                console.warn('Invariant violation 1');
+                                return;
+                            }
+
+                            parentFunc.replaceWith(r);
+                        } else if (t.isArrowFunctionExpression(n)) {
+                            const newBody = t.blockStatement([parentIdExpression, t.returnStatement(n.body)])
+                            parentFunc.replaceWith(t.arrowFunctionExpression(params, newBody));
+                        } else {
+                            console.warn('Invariant violation 2');
+                            return;
+                        }
+                    }
+
                     let args = n.arguments;
                     instanceId++;
-                    args = [t.numericLiteral(instanceId)].concat(args);
 
-                    const callee = t.memberExpression(n.callee.object, t.identifier('__createElement'));
-                    return path.replaceWith(t.callExpression(callee, args));
+                    const idExpression = getIdExpression({
+                        VAR: parentIdVarName,
+                        ID: t.stringLiteral(instanceId.toString())
+                    }).expression;
+                    args = [idExpression].concat(args);
+
+                    const callee = t.memberExpression(n.callee.object, rclCreateElementNameIdentifier);
+                    path.replaceWith(t.callExpression(callee, args));
                 }
             }
         }
--- tests/babelPlugin.test.js
+++ tests/babelPlugin.test.js
@@ -1,50 +1,69 @@
-import * as babel from 'babel-core';
-import plugin from '../rcl/babelPlugin';
+const babel = require('babel-core');
+const plugin = require('../rcl/babelPlugin');
 
-const classTestCase = `
-export default class Root extends React.Component {
+const testCase = `
+class Foo1 {
   render() {
-      return React.createElement(
-          'div',
-          null,
-          React.createElement(
-              'button',
-              { onClick: () => this.setState({}) },
-              'Hi'
-          ),
-          React.createElement(Class, { prop: 'Class' }),
-          React.createElement(PureClass, { prop: 'PureClass' }),
-          React.createElement(Func, null),
-          React.createElement(Func, { prop: 'Func1' })
-      );
+    return React.createElement(
+      A,
+      null,
+      React.createElement(B, null)
+    );
   }
 }
-`;
 
-const funcTestCase = `
-function func() {
+const foo2 = {
+  render() {
+    return React.createElement(
+      A,
+      null,
+      React.createElement(B, null)
+    );
+  }
+}
+
+const foo3 = {
+  render: function() {
+    return React.createElement(
+      A,
+      null,
+      React.createElement(B, null)
+    );
+  }
+}
+
+function Foo4() {
   return React.createElement(
-    'div',
+    A,
     null,
-    React.createElement(
-        'button',
-        { onClick: () => this.setState({}) },
-        'Hi'
-    ),
-    React.createElement(Class, { prop: 'Class' }),
-    React.createElement(PureClass, { prop: 'PureClass' }),
-    React.createElement(Func, null),
-    React.createElement(Func, { prop: 'Func1' })
+    React.createElement(B, null)
   );
 }
+
+const foo5 = function () {
+  return React.createElement(
+    A,
+    null,
+    React.createElement(B, null)
+  );
+};
+
+const foo6 = () => {
+  return React.createElement(
+    A,
+    null,
+    React.createElement(B, null)
+  );
+};
+
+const foo7 = () => React.createElement(
+  A,
+  null,
+  React.createElement(B, null)
+);
 `;
 
-it('classTestCase', () => {
-  const {code} = babel.transform(classTestCase, {plugins: [plugin]});
-  expect(code).toMatchSnapshot();
-});
-
-it('funcTestCase', () => {
-  const {code} = babel.transform(classTestCase, {plugins: [plugin]});
+it('should be awesome', () => {
+  const {code} = babel.transform(testCase, {plugins: [plugin]});
   expect(code).toMatchSnapshot();
 });
